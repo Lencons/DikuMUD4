6d7270399d83d94d2e339f91003b8361a089c4a3
Dave Lennox <david@dunbart.net>
2025-03-07 18:30:01 +1100

    Clean up and some bug fixing of the Queue system.

--- vme/src/queue.cpp
+++ vme/src/queue.cpp
@@ -1,64 +1,186 @@
-/*
- $Author: tperry $
- $RCSfile: queue.cpp,v $
- $Date: 2001/04/29 03:46:07 $
- $Revision: 2.1 $
+/** @file
+ * @brief Input and Output data Queue functionality.
+ *
+ * The buffering of data to be read from and written to clients of the
+ * application server is performed through this implementation of Queues.
+ * These Queues provide a linked list of data nodes.
+ * 
+ * This file provdes the method definitions for the two classes that make up
+ * the Queue processing:
+ *      Queue       Provides all of the functionality of the queue.
+ *      QueueElem   Container for the nodes within the list storing data.
+ * 
  */
 
-/*
-   I will make a GetTail operation if / when it is needed. In such case,
-   the linked list should be a doubly linked list for speed.
-*/
-
-#include "queue.h"
-
-#include "json_helper.h"
-#include "utility.h"
 
 #include <cassert>
 #include <cstring>
 
-cQueueElem::cQueueElem(char *pStr, int bCopy)
+#include "queue.h"
+#include "slog.h"
+
+#include "json_helper.h"
+#include "utility.h"
+
+
+/******************************************************************************
+ * @brief Constructor to create a Queue Element containing a string.
+ *
+ * Creates a new cQueueElem object containing the character string provided
+ * in str. The constructor can use the provided str pointer and use that
+ * memory reference within the element, or if copy is set to a non-zero value
+ * then the string at str will be copied to a new memory location and stored
+ * into the Queue Element.
+ *
+ * @param str       Pointer to the character string containing the source data.
+ * @param copy      Flag on if the source string is to be copied into a new
+ *                  memory location, or use the provided memory reference.
+ * @return          A new cQueueElem object.
+ *****************************************************************************/
+cQueueElem::cQueueElem(char *str, int copy)
 {
-    assert(pStr);
+    nSize = 0;
 
-    ubit32 n = strlen(pStr) + 1;
+    if ((str == nullptr) || (*str == '\0')) {
+        slog(LOG_ALL, 0, "Warning - Added empty string to Queue.");
+        nSize = 0;
+        pData = nullptr;
+    } else {
+        nSize = strlen(str) + 1;
 
-    if (bCopy)
-    {
-        CREATE(pData, ubit8, n);
-        memcpy(pData, pStr, n);
+        if (copy == 0) {
+            pData = (ubit8 *)str;
+        } else {
+            CREATE(pData, ubit8, nSize);
+            memcpy(pData, str, nSize);
+        }
     }
-    else
-    {
-        pData = (ubit8 *)pStr;
-    }
-
-    nSize = n;
 }
 
-cQueueElem::cQueueElem(ubit8 *d, ubit32 n, int bCopy)
-{
-    if (bCopy)
-    {
-        if (n > 0)
-        {
-            CREATE(pData, ubit8, n);
-            memcpy(pData, d, n);
-        }
-        else
-        {
-            pData = nullptr;
-        }
-    }
-    else
-    {
-        pData = d;
-    }
 
-    nSize = n;
+/******************************************************************************
+ * @brief Constructor to create a Queue Element containing binary data.
+ *
+ * Creates a new cQueueElem object containing the byte data provided. The
+ * constructor can use the provided data pointer and use this memory reference
+ * within the element, or if copy is set to a non-zero value then the source
+ * data will be copied to a new memory location and stored into the Queue
+ * Element.
+ *
+ * @param data      Pointer to an array of source byte data.
+ * @param len       Length of the source data array in bytes.
+ * @param copy      Flag on if the source string is to be copied into a new
+ *                  memory location, or use the provided memory reference.
+ * @return          A new cQueueElem object.
+ *****************************************************************************/
+cQueueElem::cQueueElem(ubit8 *data, ubit32 len, int copy)
+{
+    if ((data == nullptr) || (len == 0)) {
+        slog(LOG_ALL, 0, "Warning - Added empty data to Queue.");
+        nSize = 0;
+        pData = nullptr;
+    } else {
+        nSize = len;
+
+        if (copy == 0) {
+            pData = data;
+        } else {
+            CREATE(pData, ubit8, nSize);
+            memcpy(pData, data, nSize);
+        }    
+    }
 }
 
+
+/******************************************************************************
+ * @brief Destructor for a cQueueElem object.
+ *
+ * When the Queue node is destroyed, we need to make sure that any data
+ * referenced by pData is also freed to ensure we don't leak memory.
+ *****************************************************************************/
+cQueueElem::~cQueueElem()
+{
+    if (pData != nullptr) {
+        FREE(pData);
+    }
+}
+
+
+/******************************************************************************
+ * @brief Wrapper function to return the number of bytes of data stored within
+ *        the Queue.
+ *
+ * Simple wrapper function to return the total number of bytes of data stored
+ * across all nodes within the list.
+ * 
+ * @return          Number of bytes of data stored within the Queue.
+ *****************************************************************************/
+ubit32 cQueueElem::Bytes()
+{ 
+    return nSize;
+}
+
+/******************************************************************************
+ * @brief Wrapper function to return a reference to the data stored within the
+ *        Queue node.
+ *
+ * Simple wrapper function to return the pData reference to the data stored
+ * within this Queue node.
+ * 
+ * @note    Nodes within the Queue are not required to have a reference to any
+ *          data. So it is possible for a QueueElem to have a pData nullptr
+ *          reference.
+ * 
+ * @return          Pointer to the stored data array.
+ *****************************************************************************/
+ubit8 *cQueueElem::Data()
+{
+    return pData;
+}
+
+
+/******************************************************************************
+ * @brief Clears any reference to stored data for the node.
+ *
+ * Resets the QueueElem node to not reference or be storing any data by setting
+ * all data references to NULL, zero or empty string.
+ * 
+ * @note    This method just clobbers any data that this Queue node is
+ *          referencing. This is performed without updating the Queue data
+ *          content count or attempting to free any existing memory referenced
+ *          by the node. This method should be used very cautiously.
+ * 
+ *****************************************************************************/
+void cQueueElem::SetNull()
+{
+    pData = nullptr;
+    nSize = 0;
+}
+
+
+/******************************************************************************
+ * @brief Wrapper function to get the reference to next node in the Queue.
+ *
+ * Simple wrapper function to return the reference to the next QueueElem node
+ * within the list. The Queue itself and the data within the node is left
+ * unmodified.
+ * 
+ * @return          Pointer to the next node in the Queue.
+ *****************************************************************************/
+cQueueElem *cQueueElem::PeekNext()
+{
+    return pNext;
+}
+
+
+/******************************************************************************
+ * @brief Constructor to create a Queue for server input and output
+ *        communication.
+ *
+ * Creates a new empty cQueue object.
+ *
+ * @return          A new empty cQueueobject.
+ *****************************************************************************/
 cQueue::cQueue()
 {
     pHead = nullptr;
@@ -67,163 +189,298 @@ cQueue::cQueue()
     nBytes = 0;
 }
 
+
+/******************************************************************************
+ * @brief Destructor for a cQueue object.
+ *
+ * Calls Flush() to destroy any cQueueElem elements within the Queue list.
+ *****************************************************************************/
 cQueue::~cQueue()
 {
     Flush();
 }
 
+
+/******************************************************************************
+ * @brief Return the number of nodes within the cQueue list.
+ *
+ * Simple wrapper function to return the number of list nodes stored in
+ * nEntries.
+ * 
+ * @return          The value stored in nEntries.
+ *****************************************************************************/
 ubit32 cQueue::Size()
 {
     return nEntries;
 }
 
+
+/******************************************************************************
+ * @brief Return the number of bytes stored across nodes within the cQueue
+ *        list.
+ *
+ * Simple wrapper function to return the number of bytes recorded within
+ * nBytes. This is a managed aggregation of all nodes nSize value to provide
+ * a total number of bytes currently stored within the queue.
+ * 
+ * @note This byte size value includes the null terminator of any string data
+ *       that was added into the Queue. All string data is effectively
+ *       converted to byte data when pushed into the Queue, so the returned
+ *       bytes value will be larger than what was pushed as a character string.
+ * 
+ * @return          The value stored in nEntries.
+ *****************************************************************************/
 ubit32 cQueue::Bytes()
 {
     return nBytes;
 }
 
+
+/******************************************************************************
+ * @brief Wrapper function to test if there are any nodes within the Queue.
+ *
+ * Simple wrapper function to return the number of list nodes stored in
+ * nEntries.
+ * 
+ * @return          If there are nodes in the Queue, a zero value is returned,
+ *                  otherwise a non-zero value on empty Queue.
+ *****************************************************************************/
 int cQueue::IsEmpty()
 {
     return nEntries == 0;
 }
 
-void cQueue::Copy(ubit8 *data, ubit32 nLen)
+
+/******************************************************************************
+ * @brief Copy a specific number of bytes from the Queue to an external buffer.
+ *
+ * Data will be copied from the Queue into the external buffer provided by the
+ * data pointer. The nodes in the Queue will be tranversed until len bytes of
+ * data have been copied.
+ * 
+ * If the number of bytes currently contained within the Queue is less than the
+ * requested number of bytes to be copied, then the bytes beyond the copied
+ * Queue data are zeroed out.
+ * 
+ * @param data      Pointer to external data buffer.
+ * @param len       Number of bytes to copy to the external data buffer.
+ * @return          Number of bytes copied into the buffer.
+ *****************************************************************************/
+int cQueue::Copy(ubit8 *data, ubit32 len)
 {
-    assert(nLen <= nBytes);
-
-    int now = 0;
-    cQueueElem *qe = (cQueueElem *)PeekHead();
-
-    do
-    {
-        assert(qe);
-        now = MIN(nLen, qe->Bytes());
-        memcpy(data, qe->Data(), now);
-        nLen -= now;
-        data += now;
-        qe = qe->PeekNext();
-    } while (nLen > 0);
-}
-
-void cQueue::Cut(ubit32 nLen)
-{
-    if (nLen < 1)
-    {
-        return;
-    }
-
-    assert(nLen <= nBytes);
-
+    int byte_cnt = 0, copy_cnt;
     cQueueElem *qe = nullptr;
 
-    do
-    {
+    // clean the buffer, ensures that any unused bytes will be zero.
+    memset(data, 0, len);
+
+    if (IsEmpty()) {
+        return 0;
+    }
+
+    qe = (cQueueElem *)PeekHead();
+    do {
+        copy_cnt = MIN(len, qe->Bytes());
+        memcpy(data, qe->Data(), copy_cnt);
+        len -= copy_cnt;
+        data += copy_cnt;
+        byte_cnt += copy_cnt;
+        qe = qe->PeekNext();
+    } while (len > 0);
+
+    return byte_cnt;
+}
+
+
+/******************************************************************************
+ * @brief Cut (gobble) a specific number of bytes from the Queue.
+ *
+ * Cutting bytes from the Queue which is the same as gobbling them from the
+ * Queue. The number of bytes provided in len will be removed from the Queue.
+ * 
+ * @param len       Number of bytes to copy to the external data buffer.
+ * @return          Number of bytes removed from the Queue.
+ *****************************************************************************/
+int cQueue::Cut(ubit32 len)
+{
+    int cut_cnt = 0;
+    cQueueElem *qe = nullptr;
+
+    if (len < 1) {
+        return 0;
+    }
+
+    do {
+        // Remove the next node from the Queue.
         qe = GetHead();
+        if (qe == nullptr) {
+            break;
+        };
 
-        assert(qe);
-
-        if (nLen < qe->Bytes())
-        {
-            Prepend(new cQueueElem(qe->Data() + nLen, qe->Bytes() - nLen));
+        // If there is more data in the removed node, create a new node with
+        // the remaining data.
+        if (len < qe->Bytes()) {
+            Prepend(new cQueueElem(qe->Data() + len, qe->Bytes() - len));
         }
 
-        nLen -= MIN(qe->Bytes(), nLen);
+        len -= MIN(qe->Bytes(), len);
         delete qe;
-    } while (nLen > 0);
+    } while (len > 0);
+
+    return cut_cnt;
 }
 
-void cQueue::CutCopy(ubit8 *data, ubit32 nLen)
-{
-    Copy(data, nLen);
-    Cut(nLen);
-}
 
-void cQueue::Append(cQueueElem *pe)
-{
-    if (IsEmpty())
-    {
-        pHead = pTail = pe;
-    }
-    else
-    {
-        pTail->pNext = pe;
-        pTail = pe;
-    }
-
-    pe->pNext = nullptr;
-
-    nEntries++;
-    nBytes += pe->Bytes();
-}
-
-/*  Quinn's Immortal Remark...
+/******************************************************************************
+ * @brief Remove the specific number of bytes from the Queue to an external
+ *        buffer.
  *
- *  Listen...
- *  The sound of a thousand comp.sci. professors rotating in their graves.
- */
-void cQueue::Prepend(cQueueElem *pe)
+ * Data will be copied from the Queue into the external buffer provided by the
+ * data pointer with the copied data also being removed from the Queue.
+ * 
+ * If the number of bytes currently contained within the Queue is less than the
+ * requested number of bytes to be copied, then the bytes beyond the copied
+ * Queue data are zeroed out.
+ * 
+ * @param data      Pointer to external data buffer.
+ * @param len       Number of bytes to copy to the external data buffer.
+ * @return          Number of bytes copied into the buffer.
+ *****************************************************************************/
+int cQueue::CutCopy(ubit8 *data, ubit32 len)
 {
-    if (IsEmpty())
-    {
-        pe->pNext = nullptr;
-        pTail = pe;
-    }
-    else
-    {
-        pe->pNext = pHead;
-    }
-
-    pHead = pe;
-
-    nEntries++;
-    nBytes += pe->Bytes();
+    // We are assuming here that the number of Copy() bytes is the same as the
+    // number of Cut() bytes for simplicity..... this should be the case.
+    Copy(data, len);
+    return Cut(len);
 }
 
+
+/******************************************************************************
+ * @brief Add a new QueueElem node to the end of the Queue.
+ *
+ * Push a new node onto the end of the Queue.
+ * 
+ * @param node      QueueElem node to add to the end of the Queue.
+ *****************************************************************************/
+void cQueue::Append(cQueueElem *node)
+{
+    if (IsEmpty()) {
+        pHead = pTail = node;
+    } else {
+        pTail->pNext = node;
+        pTail = node;
+    }
+
+    node->pNext = nullptr;
+
+    nEntries++;
+    nBytes += node->Bytes();
+}
+
+
+/******************************************************************************
+ * @brief Add a new QueueElem node to the front/start of the Queue.
+ *
+ * Push a new node onto the head of the Queue.
+ * 
+ * @param node      QueueElem node to add to the front of the Queue.
+ *****************************************************************************/
+void cQueue::Prepend(cQueueElem *node)
+{
+    if (IsEmpty()) {
+        node->pNext = nullptr;
+        pTail = node;
+    } else {
+        node->pNext = pHead;
+    }
+
+    pHead = node;
+
+    nEntries++;
+    nBytes += node->Bytes();
+}
+
+
+/******************************************************************************
+ * @brief Wrapper function to get the reference to the head of the Queue.
+ *
+ * Simple wrapper function to return a reference to the QueueElem node at the
+ * head of the Queue. The Queue itself and the data within the Queue is left
+ * unmodified.
+ * 
+ * @return          Pointer to the first node in the Queue.
+ *****************************************************************************/
 const cQueueElem *cQueue::PeekHead()
 {
     return pHead;
 }
 
+
+/******************************************************************************
+ * @brief Wrapper function to get the reference to the tail of the Queue.
+ *
+ * Simple wrapper function to return a reference to the QueueElem node at the
+ * end of the Queue. The Queue itself and the data within the Queue is left
+ * unmodified.
+ * 
+ * @return          Pointer to the first node in the Queue.
+ *****************************************************************************/
 const cQueueElem *cQueue::PeekTail()
 {
     return pTail;
 }
 
+
+/******************************************************************************
+ * @brief Remove the node from the head of the Queue.
+ *
+ * The QueueElem node currently as the head of the Queue is popped out of the
+ * list and a reference to it is returned.
+ * 
+ * @return          Pointer to the node removed from the Queue.
+ *****************************************************************************/
 cQueueElem *cQueue::GetHead()
 {
-    cQueueElem *pTmp = nullptr;
+    cQueueElem *p = nullptr;
 
-    if (IsEmpty())
-    {
+    if (IsEmpty()) {
         return nullptr;
     }
 
-    pTmp = pHead;
+    p = pHead;
 
-    if (nEntries == 1)
-    {
+    if (nEntries == 1) {
         pHead = pTail = nullptr;
-    }
-    else
-    {
+    } else {
         pHead = pHead->pNext;
     }
 
     nEntries--;
-    nBytes -= pTmp->Bytes();
+    nBytes -= p->Bytes();
 
-    return pTmp;
+    return p;
 }
 
+
+/******************************************************************************
+ * @brief Flush the Queue, destroying all data current contained within it.
+ *
+ * Flushing the Queue removes all nodes currently within the list and resets
+ * the Queue back to a empty list.
+ * 
+ * @note    All QueueElem nodes that are removed from the Queue are destroyed
+ *          making any external references to the memory invalid.
+ * 
+ *****************************************************************************/
 void cQueue::Flush()
 {
-    cQueueElem *pTmp = nullptr;
+    cQueueElem *p = nullptr;
 
-    while ((pTmp = GetHead()))
-    {
-        delete pTmp;
+    while ((p = GetHead()) != nullptr) {
+        delete p;
     }
 }
+
 
 void cQueue::toJSON(rapidjson::PrettyWriter<rapidjson::StringBuffer> &writer) const
 {
--- vme/src/queue.h
+++ vme/src/queue.h
@@ -1,45 +1,75 @@
-/*
- $Author: tperry $
- $RCSfile: queue.h,v $
- $Date: 2001/04/29 03:46:07 $
- $Revision: 2.1 $
+/** @file
+ * @brief Client connection read and write buffer queue.
+ *
+ * Classes that implement a buffer queue as a linked list for server input
+ * and output over the hooked connections.
+ * 
+ * The queue is managed through the cQueue class which provides the normal
+ * list management functions. The data contained within the list resides
+ * within internal cQueueElem nodes that should be operated upon through
+ * cQueue.
  */
-#pragma once
+#ifndef VME_QUEUE_H
+#define VME_QUEUE_H
 
 #include "essential.h"
 
 #include <rapidjson/prettywriter.h>
 
-// To use, inherit this type into your data structure.
+
+/******************************************************************************
+ * @brief Used for the nodes within a Queue list.
+ *
+ * Used to store data within a Queue as a node within the linked list that
+ * makes up the Queue. The QueueElem node is part of a singularly linked list
+ * with a reference to the next node in the list referenced by pNext.
+ * 
+ * There is no requirement that a node within the list must reference any data
+ * so pData being a nullptr is a valid value and in which case nSize should
+ * also be zero.
+ * 
+ * Within the node, data is treated as an array of bytes of nSize length. When
+ * creating nodes using the character string constructure note that the data
+ * stored within the node will identify as one byte longer than the string as
+ * the '\0' string termination character is also added to the data array.
+ * 
+ *****************************************************************************/
 class cQueueElem
 {
     friend class cQueue;
 
 public:
-    cQueueElem(char *c, int bCopy = TRUE);
-    cQueueElem(ubit8 *d, ubit32 n, int bCopy = TRUE);
-    ~cQueueElem()
-    {
-        if (pData)
-            FREE(pData);
-    }
+    cQueueElem(char *str, int copy = TRUE);
+    cQueueElem(ubit8 *data, ubit32 len, int copy = TRUE);
+    ~cQueueElem();
 
-    ubit32 Bytes() { return nSize; }
-    ubit8 *Data() { return pData; }
-    void SetNull()
-    {
-        pData = nullptr;
-        nSize = 0;
-    }
+    ubit32 Bytes();
+    ubit8 *Data();
 
-    cQueueElem *PeekNext() { return pNext; }
+    void SetNull();
+    cQueueElem *PeekNext();
 
 private:
+    //! Reference to the array of byte data stored within the node.
     ubit8 *pData;
-    ubit32 nSize; ///< Optional number of bytes
+
+    //! Number of bytes of data stored within the byte array (it's length).
+    ubit32 nSize;
+
+    //! Reference to the next node in the list, or nullptr if the end.
     cQueueElem *pNext;
 };
 
+
+/******************************************************************************
+ * @brief Provieds a data buffer queue for use by server input and output
+ *        data streams.
+ *
+ * In managing the buffering of data streams read into and written out of the
+ * server, Queue buffers are set up handle data to be sent to the client or as
+ * as landing place for incoming data read for processing by the main server
+ * loop.
+ *****************************************************************************/
 class cQueue
 {
 public:
@@ -50,15 +80,14 @@ public:
     ubit32 Size();
     ubit32 Bytes();
 
-    void Copy(ubit8 *data, ubit32 nLen);
-    void CutCopy(ubit8 *data, ubit32 nLen);
-    void Cut(ubit32 nLen);
+    int Copy(ubit8 *data, ubit32 len);
+    int Cut(ubit32 len);
+    int CutCopy(ubit8 *data, ubit32 len);
 
-    void Append(cQueueElem *pe);
-    void Prepend(cQueueElem *pe);
+    void Append(cQueueElem *node);
+    void Prepend(cQueueElem *node);
 
     cQueueElem *GetHead();
-
     const cQueueElem *PeekHead();
     const cQueueElem *PeekTail();
 
@@ -67,10 +96,17 @@ public:
     void toJSON(rapidjson::PrettyWriter<rapidjson::StringBuffer> &writer) const;
 
 private:
+    //! Reference to the first node in the Queue list, nullptr if Queue empty.
     cQueueElem *pHead;
-    cQueueElem *pTail;
-    cQueueElem *pPreTail;
 
+    //! Reference to the last node in the Queue list, nullptr if Queue empty.
+    cQueueElem *pTail;
+
+    //! A count of the number of nodes within the Queue list.
     ubit32 nEntries;
+
+    //! A sum of all the bytes of data stored within the Queue list.
     ubit32 nBytes;
 };
+
+#endif /* VME_QUEUE_H */
